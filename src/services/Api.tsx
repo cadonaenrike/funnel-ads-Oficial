6725be144362206c8f472ddf5a80bf744b97a222
everton <cadonaenrike@gmail.com>
2024-03-08 00:59:22 -0300

    ajuste do service api para link antigo

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -6,6 +6,8 @@ const api = axios.create({
   headers: {
     "content-type": "application/json",
     "api-key": process.env.NEXT_PUBLIC_APYKEY,
+    "Access-Control-Allow-Origin":
+      "https://funnel-ads-oficial-sable.vercel.app",
   },
 });
 
