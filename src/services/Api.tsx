8bff52eee6170f231a0841ca5b6b2d8513efea91
everton <cadonaenrike@gmail.com>
2024-03-08 01:25:58 -0300

    ajuste do service api para link novo

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://funnelads.vercel.app",
-  // baseURL: "https://server-oqb147cum-softus.vercel.app", essse e o novo do time ja mas esta com erro de cors
+  // baseURL: "https://funnelads.vercel.app",
+  // baseURL: "https://server-oqb147cum-softus.vercel.app",
+  baseURL: "https://server-self-pi.vercel.app",
   headers: {
     "content-type": "application/json",
     "api-key": process.env.NEXT_PUBLIC_APYKEY,
