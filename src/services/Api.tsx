c2d910ff52cf11c70a7c70943796ac7dac5dc594
everton <cadonaenrike@gmail.com>
2024-03-07 23:06:01 -0300

    agora vai

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://funnelads.vercel.app/",
-  // baseURL: "http://localhost:5000/",
+  baseURL: "https://server-oqb147cum-softus.vercel.app/",
   headers: {
     "content-type": "application/json",
     "api-key": process.env.NEXT_PUBLIC_APYKEY,
