c74293774102dad5f23004a469786655a72840f4
everton <cadonaenrike@gmail.com>
2024-03-07 23:47:07 -0300

    ajuste do service api

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://server-oqb147cum-softus.vercel.app/",
+  baseURL: "https://server-oqb147cum-softus.vercel.app",
   headers: {
     "content-type": "application/json",
     "api-key": process.env.NEXT_PUBLIC_APYKEY,
